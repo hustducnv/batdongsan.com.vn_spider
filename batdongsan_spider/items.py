2b118c7452e92e51445a22a62ce9dad2712370b5
hustducnv <hust.ducnv@gmail.com>
2020-12-10 20:40:40 +0700

    first commit

---
+++ batdongsan_spider/items.py
@@ -0,0 +1,35 @@
+from scrapy import Item, Field
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+import re
+
+
+def clean_input(value):
+    value = value.strip()
+    if value != '':
+        return value
+    return None
+
+
+def get_number(text):
+    numbers = re.findall(r'\d{1,3}', text)
+    if numbers is not None:
+        return numbers[0]
+
+
+class ApartmentItem(Item):
+    url = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    title = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    short_detail = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    price = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    area = Field(input_processor=MapCompose(clean_input, get_number), output_processor=TakeFirst())
+    bedrooms = Field(input_processor=MapCompose(clean_input, get_number), output_processor=TakeFirst())
+    toilets = Field(input_processor=MapCompose(clean_input, get_number), output_processor=TakeFirst())
+    description = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    address = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    balcony_direction = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    direction = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    furniture = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    investor = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    law_doc = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    project = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
+    post_date = Field(input_processor=MapCompose(clean_input), output_processor=TakeFirst())
---
+++ batdongsan_spider/spiders/spider.py
@@ -0,0 +1,44 @@
+from scrapy import Spider, Request
+from scrapy.loader import ItemLoader
+from ..items import ApartmentItem
+
+
+class DBSspider(Spider):
+    name = 'BDSspider'
+    # allowed_domains = ['https://batdongsan.com.vn/']
+
+    def start_requests(self):
+        start_url = 'https://batdongsan.com.vn/ban-can-ho-chung-cu-ha-noi'
+        for i in range(1, 886):
+            yield Request(url=start_url + '/p' + str(i), callback=self.parse_link)
+
+        # one page
+        # url = 'https://batdongsan.com.vn/ban-can-ho-chung-cu-duong-5-xa-dong-hoi-prj-eurowindow-river-park/chi-23-7tr-m2-91m2-3pn-2vs-full-noi-that-co-ban-view-nam-h-long-bien-pr28106294'
+        # yield Request(url, callback=self.parse_features)
+
+    def parse_link(self, response):
+        # base_url = 'https://batdongsan.com.vn'
+        apartment_links = response.xpath('//*[@id="product-lists-web"]/div/a')
+        yield from response.follow_all(apartment_links, callback=self.parse_features)
+
+    def parse_features(self, response):
+        l = ItemLoader(item=ApartmentItem(), response=response)
+
+        l.add_value('url', response.url)
+        l.add_css('title', '#product-detail-web > h1.tile-product::text')
+        l.add_css('short_detail', '#product-detail-web > div.short-detail::text')
+        l.add_xpath('price', '//*[@id="product-detail-web"]/div[3]/ul/li[contains(span[1]/text(), "giá")]/span[2]/text()')
+        l.add_xpath('area', '//*[@id="product-detail-web"]/div[3]/ul/li[contains(span[1]/text(), "Diện tích")]/span[2]/text()')
+        l.add_xpath('bedrooms', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "phòng ngủ")]/span[2]/text()')
+        l.add_xpath('toilets', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "toilet")]/span[2]/text()')
+        l.add_css('description', '.des-product::text')
+        l.add_xpath('address', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "Địa chỉ")]/span[2]/text()')
+        l.add_xpath('direction', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "Hướng nhà")]/span[2]/text()')
+        l.add_xpath('balcony_direction', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "Hướng ban công")]/span[2]/text()')
+        l.add_xpath('furniture', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "Nội thất")]/span[2]/text()')
+        l.add_xpath('law_doc', '//*[@id="product-detail-web"]/div[5]/div[2]/div/div[contains(span[1]/text(), "Pháp lý")]/span[2]/text()')
+        l.add_xpath('project', '//*[@id="product-detail-web"]/div[5]/div[3]/div/div[contains(span[1]/text(), "Tên")]/span[2]/text()[1]')
+        l.add_xpath('investor', '//*[@id="product-detail-web"]/div[5]/div[3]/div/div[contains(span[1]/text(), "Chủ")]/span[2]/text()[1]')
+        l.add_xpath('post_date', '//*[@id="product-detail-web"]/div[5]/div[8]/ul/li[1]/span[2]/text()')
+
+        yield l.load_item()
